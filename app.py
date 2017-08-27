ab83fb3b1cb02241e8ed4fe69029445a5d24045c
unknown <sammysteppa90@gmail.com>
2017-08-28 01:15:03 +0300

    Build fix for travis ci and code climate

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hallo world flask"
+    return render_template("Designs/index.html")
 
 app.run(debug=True)

05f5d30039bf374a5063b401df4b384a25eaed71
mirr254 <sammysteppa90@gmail.com>
2017-08-31 05:00:15 +0300

    [NEW FEATURE] Adds new feature for dashboard to include form

--- run.py
+++ run.py
@@ -3,25 +3,30 @@ from app.models import addUser, system_users
 
 app = Flask(__name__ ,template_folder='Designs')
 
-#kepp track of logins
-loggedin = False
+
 
 @app.route("/")
 def index():
-    if is loggedin:
-        return render_template("dashboard.html")
+    
     return render_template("index.html")
 
-@app.route("/signup/", methods=['POST'])
+@app.route("/signup/", methods=['POST','GET'])
 def signupUser():
-    password =request.form['password']
-    email =request.form['email']
-    fname =request.form['fname']    
-    password_confirm =request.form['password_confirm']
 
-    if password != password_confirm:
-        err = "Password Mismatch"
-        return render_template("signup.html", err = err )    
+    if request.method == "GET":
+        return render_template("dashboard.html")
+
+    #post request
+    elif request.method() == "POST":
+        
+        password =request.form['password']
+        email =request.form['email']
+        fname =request.form['fname']    
+        password_confirm =request.form['password_confirm']
+
+        if password != password_confirm:
+            err = "Password Mismatch"
+            return render_template("signup.html", err = err )    
     
     addUser(fname, email, password)
     loggedin = True
@@ -42,25 +47,19 @@ check if user exists
 """
 @app.route("/dashboard", methods=["POST", "GET"])
 def dashboard():
-    if method.request() == "GET":
-        #check if user is logged in
-        if loggedin:
-            return render_template("dashboard.html")
+    #methd is post
+    password=request.form['password']
+    email=request.form['email']
+    #check if user exists
+    if email in system_users.keys():
+        loggedin = True
+        return render_template("dashboard.html", email=email)
 
-        else:
-            return render_template("index.html")
     else:
-        #methd is post
-        password=request.form['password']
-        email=request.form['email']
-        #check if user exists
-        if email in system_users.keys():
-            loggedin = True
-            return render_template("dashboard.html", email=email)
-
-        else:
-            err = "Email Mismatch"
-            return render_template("index.html" err=err)
+        err = "Email Mismatch"
+        return render_template("index.html", err=err)
+    
+        
 
 @app.route("/reset-password")
 def passwdReset():

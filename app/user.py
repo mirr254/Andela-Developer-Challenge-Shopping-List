2a293cd5430303d494adb5d45f41c000b30771d9
mirr254 <sammysteppa90@gmail.com>
2017-09-04 16:49:17 +0300

    [BUG FIX] Fix user module not being found

---
+++ app/user.py
@@ -0,0 +1,12 @@
+#this script handles the operations to be performed on user accounts
+#for example adding user or deleting user
+
+class User(object):
+
+    #initialize the class
+    def __init__(self,username, first_name,last_name, email, password):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name        
+        self.email = email
+        self.password = password
